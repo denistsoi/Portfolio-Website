8fde36cbb1295d0a1b1ef44ca8bd59fe7b3336b7
denistsoi <denistsoi@gmail.com>
2014-09-02 16:00:53 +0800

    ADD: [project] gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,31 @@
-// var gulp = require('gulp'),
-//     sass = require('gulp-sass'),
-//     connect = require('gulp-connect');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    clean = require('gulp-clean'),
+    connect = require('gulp-connect');
 
-// gulp.task('default')
+var src = {
+  scss: "src/styles/scss/",
+  css:  "src/styles/css/"
+}
+
+var build = {
+  css:  "public/css/"
+}
+
+gulp.task('dev', function() {
+  connect.server({
+    livereload: true
+  });
+});
+
+gulp.task('clean-css', function() {
+  gulp.src(src.css)
+    .pipe(clean());
+});
+
+gulp.task('sass', ['clean-css'], function() {
+  gulp.src(src.scss+"*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest(src.css))
+    .pipe(connect.reload());
+});
