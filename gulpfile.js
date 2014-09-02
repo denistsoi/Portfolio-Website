8c324eef54e0181c48d904280ed15879c074d9dd
denistsoi <denistsoi@gmail.com>
2014-09-02 16:11:34 +0800

    ADD: [project] gulp watch project task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     clean = require('gulp-clean'),
+    watch = require('gulp-watch'),
     connect = require('gulp-connect');
 
 var src = {
@@ -18,12 +19,20 @@ gulp.task('dev', function() {
   });
 });
 
+gulp.task('default', ['build']);
+
+gulp.task('build', ['clean-css', 'sass', 'watch']);
+
+gulp.task('watch', function(){
+  watch(src.scss+"*.scss", ['sass']);
+});
+
 gulp.task('clean-css', function() {
   gulp.src(src.css)
     .pipe(clean());
 });
 
-gulp.task('sass', ['clean-css'], function() {
+gulp.task('sass', function() {
   gulp.src(src.scss+"*.scss")
     .pipe(sass())
     .pipe(gulp.dest(src.css))
